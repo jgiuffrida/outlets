19b3335d9f2c1d71a8ba905eda6500692f6ac337
Joe Giuffrida <jm.giuffrida@gmail.com>
2016-10-09 21:02:27 -0400

    add some test stuff

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var outlets = require('./routes/outlets');
 
 var app = express();
+var expressWs = require('express-ws')(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/outlets', outlets);
+app.use('/outlets', outlets(expressWs));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/outlets.js
+++ routes/outlets.js
@@ -2,30 +2,41 @@ var express = require('express');
 var router = express.Router();
 var OutletManager = require('../outlets/manager');
 
-/* GET outlets listing. */
-router.get('/', function(req, res, next) {
-    res.json(OutletManager.getStatus());
-});
+module.exports = function (expressWs) {
+    router.ws('/status', (ws, req) => {
+        ws.on('status', () => {
+            ws.send(OutletManager.getStatus());
+        });
+    });
 
-// get status of an outlet?
-router.get('/:outletId', function(req, res, next) {
-    res.json(OutletManager.getOutletStatus(req.params.outletId));
-});
+    var wss = expressWs.getWss('/status');
+    console.log(wss);
+    
+    /* GET outlets listing. */
+    router.get('/', function (req, res, next) {
+        res.json(OutletManager.getStatus());
+    });
 
-// Post to on/off outlets
-router.post('/:outletId', (req, res, next) => {
-    OutletManager.setOutletStatus(req.params.outletId, req.body.value);   
-    res.json(OutletManager.getOutletStatus(req.params.outletId));
-});
+    // get status of an outlet?
+    router.get('/:outletId', function (req, res, next) {
+        res.json(OutletManager.getOutletStatus(req.params.outletId));
+    });
 
-router.get('/:outletId/on', (req, res, next) => {
-    OutletManager.setOutletStatus(req.params.outletId, true);
-    res.json(OutletManager.getOutletStatus(req.params.outletId));
-});
+    // Post to on/off outlets
+    router.post('/:outletId', (req, res, next) => {
+        OutletManager.setOutletStatus(req.params.outletId, req.body.value);
+        res.json(OutletManager.getOutletStatus(req.params.outletId));
+    });
 
-router.get('/:outletId/off', (req, res, next) => {
-    OutletManager.setOutletStatus(req.params.outletId, false);
-    res.json(OutletManager.getOutletStatus(req.params.outletId));
-});
+    router.get('/:outletId/on', (req, res, next) => {
+        OutletManager.setOutletStatus(req.params.outletId, true);
+        res.json(OutletManager.getOutletStatus(req.params.outletId));
+    });
 
-module.exports = router;
+    router.get('/:outletId/off', (req, res, next) => {
+        OutletManager.setOutletStatus(req.params.outletId, false);
+        res.json(OutletManager.getOutletStatus(req.params.outletId));
+    });
+
+    return router;
+};
