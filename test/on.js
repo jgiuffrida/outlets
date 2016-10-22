0cf55eea30643e2a74b995cc986bc60256eb9a0b
Joe Giuffrida <jm.giuffrida@gmail.com>
2016-10-22 17:11:23 -0400

    adding on and off scripts

---
+++ test/on.js
@@ -0,0 +1,16 @@
+var OutletManager = require('../outlets/manager'),
+    _ = require('lodash'),
+    testOutlets = [1,2,3,4,5,6,7,8,10,11,12,13,14,15,16],
+    idx = 0,
+    delta = 1;
+
+var interval = setInterval(() => {
+    let outlet = testOutlets[idx];
+    OutletManager.setOutletStatus(outlet, true);
+    
+
+    idx = idx+delta;
+    if(idx === testOutlets.length) {
+        clearInterval(interval);
+    }
+},200);
